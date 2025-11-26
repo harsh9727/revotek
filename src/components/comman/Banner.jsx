b0c756397862e4be79977120a2d6c57780a4e840
harsh.sinontechs <harsh.sinontechs@gmail.com>
2025-11-26 17:17:14 +0530

    comman banner responsive

--- src/components/comman/Banner.jsx
+++ src/components/comman/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = ({ title }) => {
       <img src={productBanner} alt="product-banner" loading="lazy" data-aos="fade-down" />
       <Container>
         <div className="commanbanner-content " >
-          <h3 data-aos="fade-right">{title}</h3>
+          {/* <h3 data-aos="fade-right">{title}</h3> */}
           <Breadcrumb data-aos="fade-right">
             <Breadcrumb.Item href="/">
               <IoHomeOutline /> Home
